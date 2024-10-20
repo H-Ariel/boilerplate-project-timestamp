3403c5722245ebe254ee21adfa8de0da15069769
H-Ariel <127103172+H-Ariel@users.noreply.github.com>
2024-10-20 09:58:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,10 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/:date?', function(req, res) {
+app.get('/api/:date?', function (req, res) {
   let date_string = req.params.date;
   let date = null;
   if (/^\d{4}-\d{1,2}-\d{1,2}$/.test(date_string))
@@ -34,7 +34,7 @@ app.get('/api/:date?', function(req, res) {
   else if (date_string == undefined)
     date = new Date();
 
-  res.json( date ? {
+  res.json(date ? {
     unix: Math.floor(date.getTime()),
     utc: date.toUTCString()
   } : {
